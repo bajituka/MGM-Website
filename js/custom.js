a69f015b602e5ac22fcd3e34cebe88315eff10f9
Mikhail Terentiev <anhungered@gmail.com>
2015-11-20 16:45:57 +0300

    Revert "First commit"
    This reverts commit 0672459af79f608ec95859545533e1c8d3fd6a48.

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,6 @@
 $(document).ready(function() {
-	
 	$(".nav a").click(function(event){		
 		event.preventDefault();
 		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 400);
-	});
-	$(".btn").click(function(){
-		var releaseCount = $("#counter").html();
-		$("#counter").html(+releaseCount+1);
-		if (releaseCount == 1) {
-			$("#satisfactionLow").toggleClass("hidden show", 500).toggleClass("hidden", 2000)}
-			else if (releaseCount == 2) {
-				$("#satisfactionMedium").toggleClass("hidden show").toggleClass("hidden", 2000)}
-			else if (releaseCount == 3) {
-				$("#satisfactionHigh").toggleClass("hidden show", 500)}
 	});
 });
